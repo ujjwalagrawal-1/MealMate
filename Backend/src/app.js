1fb05236723186451ef702392ad5673f6ff6b584
ujjwalagrawal-1 <kumarujjwalagrawal4@gmail.com>
2024-12-02 17:18:28 +0530

    Some Addition files Added

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,23 +1,23 @@
 import express from 'express';
 import cors from 'cors';
-// import bodyParser from 'body-parser';
-// import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 
 // Initialize express app
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware setup
-// app.use(cors({
-//     origin: process.env.CORS_ORIGIN,
-//     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-//     credentials: true
-// }));
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    credentials: true
+}));
 
-// app.use(cookieParser());
-// app.use(express.json({ limit: "16kb" }));
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static("public"));
+app.use(cookieParser());
+app.use(express.json({ limit: "16kb" }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 // // Routes
 
