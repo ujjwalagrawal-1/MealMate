40cf4d42ba276b1bbb2de465287843cb2708aec3
ujjwalagrawal-1 <kumarujjwalagrawal4@gmail.com>
2024-12-15 03:31:25 +0530

    Some changes

--- Backend/src/models/Mess.js
+++ Backend/src/models/Mess.js
@@ -6,13 +6,7 @@ const MessSchema = new mongoose.Schema({
   mealTimes: [{ type: String, enum: ["Breakfast", "Lunch", "Dinner"] }],
   isActive: { type: Boolean, default: false },
   collaborators: [{ type: mongoose.Schema.Types.ObjectId, ref: "Admin" }],
-<<<<<<< HEAD
-  WardenId: { type: mongoose.Schema.Types.ObjectId, ref: "Admin" },
-  qrCodeUrl: { type: String },
-=======
   wardenId: { type: mongoose.Schema.Types.ObjectId, ref: "Admin" },
-  
->>>>>>> 16dd4942961d0abb852b02a405d902589e28c61f
 });
 
 const Mess = mongoose.model("Mess", MessSchema);
